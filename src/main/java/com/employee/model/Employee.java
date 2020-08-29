39261106bf2303e951b7c58107bfce253f89da29
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-29 13:58:54 -0400

    Create JPA Entity - Employee

---
+++ src/main/java/com/employee/model/Employee.java
@@ -0,0 +1,35 @@
+package com.employee.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String emailId;
+}
