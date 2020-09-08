9d446a847c284bf60e17a2bd37f32adcac0d7f6b
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-08 17:55:40 -0400

    change generation type to IDENTITY

--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name", nullable = false)
